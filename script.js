7985391bffaa85d507f6a1498bb55338f4697cc3
Gokila B <gokilab@amzetta.co.in>
2021-05-28 21:06:59 +0530

    added displaying numeric time format

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ const secondsHand = document.querySelector('[data-seconds-hand]');
 
 function setClock(){
     const currentDate = new Date();
+    let hours = currentDate.getHours();
+    let minute = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let session = "AM";
+    if (hours > 12){
+        hours = hours - 12;
+        session = "PM";
+    } 
+    hours = (hours < 10 ) ? "0" + hours : hours;
+    minute = (minute < 10 ) ? "0" + minute : minute;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    document.getElementById("hourSpan").innerHTML = hours;
+    document.getElementById("minSpan").innerHTML = minute;
+    document.getElementById("secondSpan").innerHTML = seconds;
+    document.getElementById("sessionSpan").innerHTML = session;
+
     const secondsRatio = currentDate.getSeconds()/60;
     const minutesRatio = (secondsRatio + currentDate.getMinutes())/60
     const hoursRatio = (minutesRatio + currentDate.getHours()) /12;
